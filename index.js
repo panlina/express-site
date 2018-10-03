9b285d27962257454898746515893cc33f2a006e
Jack Lu <jacklu@jacklu.me>
2018-10-03 13:36:52 +0800

    +--port

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var http = require('http');
 var express = require('express');
+var commander = require('commander');
+commander.option('--port <port>', undefined, Number, 80);
+commander.parse(process.argv);
 var app = express();
 var server = http.createServer(app);
-server.listen(80);
+server.listen(commander.port);
