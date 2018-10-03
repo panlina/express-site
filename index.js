981e21c7371de8f1060bed7bb63390939c2fddaa
Jack Lu <jacklu@jacklu.me>
2018-10-03 17:21:52 +0800

    +ssl

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var express = require('express');
 var commander = require('commander');
-commander.option('--port <port>', undefined, Number, 80);
+commander
+	.option('--port <port>', undefined, Number)
+	.option('--ssl')
+	.option('--cert <cert>')
+	.option('--key <key>');
 commander.parse(process.argv);
 var app = express();
-var server = http.createServer(app);
-server.listen(commander.port);
+var server =
+	commander.ssl ?
+		https.createServer({
+			cert: fs.readFileSync(commander.cert),
+			key: fs.readFileSync(commander.key)
+		}, app) :
+		http.createServer(app);
+server.listen(commander.port || (commander.ssl ? 443 : 80));
