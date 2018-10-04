5356ad449a5eb681c9905ad270510322cf7016fd
Jack Lu <jacklu@jacklu.me>
2018-10-04 22:14:38 +0800

    +proxy

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 var express = require('express');
+var httpProxy = require('http-proxy');
+var HttpProxyRules = require('http-proxy-rules');
 var commander = require('commander');
 commander
 	.option('--port <port>', undefined, Number)
 	.option('--ssl')
 	.option('--cert <cert>')
-	.option('--key <key>');
+	.option('--key <key>')
+	.option('--proxy-rules <proxy-rules>');
 commander.parse(process.argv);
+var proxy = httpProxy.createProxyServer();
+var proxyRules = new HttpProxyRules(
+	commander.proxyRules ?
+		JSON.parse(fs.readFileSync(commander.proxyRules, { encoding: "utf-8" })) :
+		{ rules: {} }
+);
 var app = express();
+app.use(function (req, res, next) {
+	var target = proxyRules.match(req);
+	if (target)
+		proxy.web(req, res, { target: target });
+	else
+		next();
+});
 var server =
 	commander.ssl ?
 		https.createServer({
