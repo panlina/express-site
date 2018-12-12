e57b3114fa38dd2fc4b7592f768dd1e42b9691e7
Jack Lu <jacklu@jacklu.me>
2018-12-12 23:25:55 +0800

    extract AdminApp

---
+++ AdminApp.js
@@ -0,0 +1,311 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var npm = require('global-npm');
+var jsonBodyParser = bodyParser.json({ strict: false });
+function AdminApp(site, { cors, auth }) {
+	var app = express();
+	if (cors)
+		app.use(cors);
+	if (auth)
+		app.use(auth);
+	app
+		.get("/proxy-rule/", (req, res, next) => {
+			res.json(site.proxyRule);
+		})
+		.get("/proxy-rule/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.proxyRule[req.params.name];
+			if (!p) {
+				res.sendStatus(404);
+				return;
+			}
+			res.json(p);
+		})
+		.put("/proxy-rule/:name", jsonBodyParser, (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.proxyRule[req.params.name];
+			site.proxyRule[req.params.name] = req.body;
+			res.status(p ? 200 : 201);
+			if (!p)
+				res.header('Location', `/proxy-rule/${encodeURIComponent(req.params.name || 'default')}`);
+			res.end();
+		})
+		.delete("/proxy-rule/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.proxyRule[req.params.name];
+			if (!p) {
+				res.sendStatus(404);
+				return;
+			}
+			delete site.proxyRule[req.params.name];
+			res.status(204).end();
+		})
+		.move("/proxy-rule/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.proxyRule[req.params.name];
+			if (!p) {
+				res.sendStatus(404);
+				return;
+			}
+			var destination = req.header('Destination');
+			var destination = destination.substr("/proxy-rule/".length);
+			var destination = decodeURIComponent(destination);
+			if (destination == 'default') destination = '';
+			var q = site.proxyRule[destination];
+			if (q)
+				if (req.header('Overwrite') == 'F') {
+					res.status(412).end();
+					return;
+				}
+			delete site.proxyRule[req.params.name];
+			site.proxyRule[destination] = p;
+			res.status(q ? 204 : 201);
+			if (!q)
+				res.header('Location', `/proxy-rule/${encodeURIComponent(destination || 'default')}`);
+			res.end();
+		})
+		.get("/vhost/", (req, res, next) => {
+			res.json(site.vhost);
+		})
+		.get("/vhost/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.vhost[req.params.name];
+			if (!p) {
+				res.sendStatus(404);
+				return;
+			}
+			res.json(p);
+		})
+		.put("/vhost/:name", jsonBodyParser, (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.vhost[req.params.name];
+			site.vhost[req.params.name] = req.body;
+			res.status(p ? 200 : 201);
+			if (!p)
+				res.header('Location', `/vhost/${encodeURIComponent(req.params.name || 'default')}`);
+			res.end();
+		})
+		.delete("/vhost/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.vhost[req.params.name];
+			if (!p) {
+				res.sendStatus(404);
+				return;
+			}
+			delete site.vhost[req.params.name];
+			res.status(204).end();
+		})
+		.move("/vhost/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var p = site.vhost[req.params.name];
+			if (!p) {
+				res.sendStatus(404);
+				return;
+			}
+			var destination = req.header('Destination');
+			var destination = destination.substr("/vhost/".length);
+			var destination = decodeURIComponent(destination);
+			if (destination == 'default') destination = '';
+			var q = site.vhost[destination];
+			if (q)
+				if (req.header('Overwrite') == 'F') {
+					res.status(412).end();
+					return;
+				}
+			delete site.vhost[req.params.name];
+			site.vhost[destination] = p;
+			res.status(q ? 204 : 201);
+			if (!q)
+				res.header('Location', `/vhost/${encodeURIComponent(destination || 'default')}`);
+			res.end();
+		})
+		.get("/app/", (req, res, next) => {
+			var json = {};
+			for (var name in site.app)
+				json[name] = serialize(site.app[name]);
+			res.json(json);
+		})
+		.get("/app/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var a = site.app[req.params.name];
+			if (!a) {
+				res.sendStatus(404);
+				return;
+			}
+			res.json(serialize(a));
+		})
+		.put("/app/:name", jsonBodyParser, (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var a = site.app[req.params.name];
+			if (a && a.running) {
+				res.status(409).send("The app is running. Stop it and try again.");
+				return;
+			}
+			site.app[req.params.name] = new this.App(req.body);
+			res.status(a ? 200 : 201);
+			if (!a)
+				res.header('Location', `/app/${encodeURIComponent(req.params.name || 'default')}`);
+			res.end();
+		})
+		.delete("/app/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var a = site.app[req.params.name];
+			if (!a) {
+				res.sendStatus(404);
+				return;
+			}
+			if (a.running) {
+				res.status(409).send("The app is running. Stop it and try again.");
+				return;
+			}
+			delete site.app[req.params.name];
+			res.status(204).end();
+		})
+		.move("/app/:name", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var a = site.app[req.params.name];
+			if (!a) {
+				res.sendStatus(404);
+				return;
+			}
+			var destination = req.header('Destination');
+			var destination = destination.substr("/app/".length);
+			var destination = decodeURIComponent(destination);
+			if (destination == 'default') destination = '';
+			var b = site.app[destination];
+			if (b) {
+				if (req.header('Overwrite') == 'F') {
+					res.status(412).end();
+					return;
+				}
+				if (b.running) {
+					res.status(409).send("The app is running. Stop it and try again.");
+					return;
+				}
+			}
+			delete site.app[req.params.name];
+			site.app[destination] = a;
+			res.status(b ? 204 : 201);
+			if (!b)
+				res.header('Location', `/app/${encodeURIComponent(destination || 'default')}`);
+			res.end();
+		})
+		.post("/app/:name/start", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var a = site.app[req.params.name];
+			if (!a) {
+				res.sendStatus(404);
+				return;
+			}
+			if (a.running) {
+				res.status(409).send("The app is already running.");
+				return;
+			}
+			a.start(e => {
+				if (e instanceof Error) {
+					res.status(500).send(e.message).end();
+					return;
+				}
+				site.eventEmitter.emit('start', `/app/${encodeURIComponent(req.params.name || 'default')}`);
+				a.process.on('exit', (code, signal) => {
+					site.eventEmitter.emit('stop', `/app/${encodeURIComponent(req.params.name || 'default')}`, { code, signal });
+				});
+				res.status(204).end();
+			});
+		})
+		.post("/app/:name/stop", (req, res, next) => {
+			if (req.params.name == 'default') req.params.name = '';
+			var a = site.app[req.params.name];
+			if (!a) {
+				res.sendStatus(404);
+				return;
+			}
+			if (!a.running) {
+				res.status(409).send("The app is not running.");
+				return;
+			}
+			a.stop(() => {
+				res.status(204).end();
+			});
+		})
+		.get("/module/", (req, res, next) => {
+			res.json(site.module);
+		})
+		.get("/module/:name", (req, res, next) => {
+			var m = site.module[req.params.name];
+			if (!m) {
+				res.sendStatus(404);
+				return;
+			}
+			res.json(m);
+		})
+		.post("/module/", jsonBodyParser, (req, res, next) => {
+			npm.load({ prefix: path.join(site.config.dir, "site_modules") }, function (er) {
+				if (er) {
+					res.status(500).send(er);
+					return;
+				}
+				npm.commands.install([req.body.source], function (er, data) {
+					if (er) {
+						res.status(500).send(er);
+						return;
+					}
+					var [, dir] = data[data.length - 1];
+					var name = path.basename(dir);
+					site.module[name] = req.body;
+					res.status(201).header('Location', `/module/${encodeURIComponent(name)}`).end();
+				});
+			});
+		})
+		.delete("/module/:name", jsonBodyParser, (req, res, next) => {
+			var m = site.module[req.params.name];
+			if (!m) {
+				res.sendStatus(404);
+				return;
+			}
+			npm.load({ prefix: path.join(site.config.dir, "site_modules") }, function (er) {
+				if (er) {
+					res.status(500).send(er);
+					return;
+				}
+				npm.commands.uninstall([req.params.name], function (er) {
+					if (er) {
+						res.status(500).send(er);
+						return;
+					}
+					delete site.module[req.params.name];
+					res.status(204).end();
+				});
+			});
+		})
+	function serialize(app) {
+		return {
+			type: app.type,
+			module: app.module,
+			arguments: app.arguments,
+			port: app.port,
+			mount: app.mount,
+			running: app.running
+		};
+	}
+	app
+		.get("/event/", (req, res, next) => {
+			res.writeHead(200, {
+				"Content-Type": "text/event-stream"
+			});
+			listen('start');
+			listen('stop');
+			function listen(type) {
+				site.eventEmitter.on(type, listener);
+				res.on('close', () => { site.eventEmitter.removeListener(type, listener); });
+				function listener(source, data) {
+					res.write(`event: ${type}\n`);
+					res.write(`data: ${source !== undefined ? source : ''}\n`);
+					res.write(`data: ${data !== undefined ? JSON.stringify(data) : ''}\n\n`);
+				}
+			}
+		})
+	return app;
+}
+module.exports = AdminApp;
--- index.js
+++ index.js
@@ -2,14 +2,13 @@ var fs = require('fs');
 var path = require('path');
 var EventEmitter = require('events');
 var express = require('express');
-var bodyParser = require('body-parser');
 var cors = require('cors');
 var basicAuth = require('express-basic-auth');
 var httpProxy = require('http-proxy');
 var HttpProxyRules = require('./HttpProxyRules');
 var createServer = require('create-server');
-var npm = require('global-npm');
 var App = require('./App');
+var AdminApp = require('./AdminApp');
 var Storage = require('./Storage');
 class Site {
 	constructor(config) {
@@ -30,7 +29,6 @@ class Site {
 			};
 		if (!fs.existsSync(path.join(config.dir, 'require.resolve.js')))
 			fs.writeFileSync(path.join(config.dir, 'require.resolve.js'), fs.readFileSync(path.join(__dirname, 'require.resolve.js')));
-		var jsonBodyParser = bodyParser.json({ strict: false });
 		var proxy = httpProxy.createProxyServer(
 			fs.existsSync(path.join(config.dir, 'proxyOptions.json')) ?
 				JSON.parse(fs.readFileSync(path.join(config.dir, 'proxyOptions.json'), { encoding: "utf-8" })) :
@@ -92,311 +90,13 @@ class Site {
 		});
 		var server = createServer(app, config.ssl ? serverOptions : undefined);
 		server.listen(config.port || (config.ssl ? 443 : 80));
-		var adminApp = express();
-		if (fs.existsSync(path.join(config.dir, 'adminCors.json')))
-			adminApp
-				.use(cors(JSON.parse(fs.readFileSync(path.join(config.dir, 'adminCors.json'), { encoding: "utf-8" }))));
-		if (fs.existsSync(path.join(config.dir, 'adminBasicAuth.json')))
-			adminApp
-				.use(basicAuth(JSON.parse(fs.readFileSync(path.join(config.dir, 'adminBasicAuth.json'), { encoding: "utf-8" }))));
-		adminApp
-			.get("/proxy-rule/", (req, res, next) => {
-				res.json(proxyRule);
-			})
-			.get("/proxy-rule/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = proxyRule[req.params.name];
-				if (!p) {
-					res.sendStatus(404);
-					return;
-				}
-				res.json(p);
-			})
-			.put("/proxy-rule/:name", jsonBodyParser, (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = proxyRule[req.params.name];
-				proxyRule[req.params.name] = req.body;
-				res.status(p ? 200 : 201);
-				if (!p)
-					res.header('Location', `/proxy-rule/${encodeURIComponent(req.params.name || 'default')}`);
-				res.end();
-			})
-			.delete("/proxy-rule/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = proxyRule[req.params.name];
-				if (!p) {
-					res.sendStatus(404);
-					return;
-				}
-				delete proxyRule[req.params.name];
-				res.status(204).end();
-			})
-			.move("/proxy-rule/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = proxyRule[req.params.name];
-				if (!p) {
-					res.sendStatus(404);
-					return;
-				}
-				var destination = req.header('Destination');
-				var destination = destination.substr("/proxy-rule/".length);
-				var destination = decodeURIComponent(destination);
-				if (destination == 'default') destination = '';
-				var q = proxyRule[destination];
-				if (q)
-					if (req.header('Overwrite') == 'F') {
-						res.status(412).end();
-						return;
-					}
-				delete proxyRule[req.params.name];
-				proxyRule[destination] = p;
-				res.status(q ? 204 : 201);
-				if (!q)
-					res.header('Location', `/proxy-rule/${encodeURIComponent(destination || 'default')}`);
-				res.end();
-			})
-			.get("/vhost/", (req, res, next) => {
-				res.json(vhost);
-			})
-			.get("/vhost/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = vhost[req.params.name];
-				if (!p) {
-					res.sendStatus(404);
-					return;
-				}
-				res.json(p);
-			})
-			.put("/vhost/:name", jsonBodyParser, (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = vhost[req.params.name];
-				vhost[req.params.name] = req.body;
-				res.status(p ? 200 : 201);
-				if (!p)
-					res.header('Location', `/vhost/${encodeURIComponent(req.params.name || 'default')}`);
-				res.end();
-			})
-			.delete("/vhost/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = vhost[req.params.name];
-				if (!p) {
-					res.sendStatus(404);
-					return;
-				}
-				delete vhost[req.params.name];
-				res.status(204).end();
-			})
-			.move("/vhost/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var p = vhost[req.params.name];
-				if (!p) {
-					res.sendStatus(404);
-					return;
-				}
-				var destination = req.header('Destination');
-				var destination = destination.substr("/vhost/".length);
-				var destination = decodeURIComponent(destination);
-				if (destination == 'default') destination = '';
-				var q = vhost[destination];
-				if (q)
-					if (req.header('Overwrite') == 'F') {
-						res.status(412).end();
-						return;
-					}
-				delete vhost[req.params.name];
-				vhost[destination] = p;
-				res.status(q ? 204 : 201);
-				if (!q)
-					res.header('Location', `/vhost/${encodeURIComponent(destination || 'default')}`);
-				res.end();
-			})
-			.get("/app/", (req, res, next) => {
-				var json = {};
-				for (var name in app)
-					json[name] = serialize(app[name]);
-				res.json(json);
-			})
-			.get("/app/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var a = app[req.params.name];
-				if (!a) {
-					res.sendStatus(404);
-					return;
-				}
-				res.json(serialize(a));
-			})
-			.put("/app/:name", jsonBodyParser, (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var a = app[req.params.name];
-				if (a && a.running) {
-					res.status(409).send("The app is running. Stop it and try again.");
-					return;
-				}
-				app[req.params.name] = new this.App(req.body);
-				res.status(a ? 200 : 201);
-				if (!a)
-					res.header('Location', `/app/${encodeURIComponent(req.params.name || 'default')}`);
-				res.end();
-			})
-			.delete("/app/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var a = app[req.params.name];
-				if (!a) {
-					res.sendStatus(404);
-					return;
-				}
-				if (a.running) {
-					res.status(409).send("The app is running. Stop it and try again.");
-					return;
-				}
-				delete app[req.params.name];
-				res.status(204).end();
-			})
-			.move("/app/:name", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var a = app[req.params.name];
-				if (!a) {
-					res.sendStatus(404);
-					return;
-				}
-				var destination = req.header('Destination');
-				var destination = destination.substr("/app/".length);
-				var destination = decodeURIComponent(destination);
-				if (destination == 'default') destination = '';
-				var b = app[destination];
-				if (b) {
-					if (req.header('Overwrite') == 'F') {
-						res.status(412).end();
-						return;
-					}
-					if (b.running) {
-						res.status(409).send("The app is running. Stop it and try again.");
-						return;
-					}
-				}
-				delete app[req.params.name];
-				app[destination] = a;
-				res.status(b ? 204 : 201);
-				if (!b)
-					res.header('Location', `/app/${encodeURIComponent(destination || 'default')}`);
-				res.end();
-			})
-			.post("/app/:name/start", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var a = app[req.params.name];
-				if (!a) {
-					res.sendStatus(404);
-					return;
-				}
-				if (a.running) {
-					res.status(409).send("The app is already running.");
-					return;
-				}
-				a.start(e => {
-					if (e instanceof Error) {
-						res.status(500).send(e.message).end();
-						return;
-					}
-					eventEmitter.emit('start', `/app/${encodeURIComponent(req.params.name || 'default')}`);
-					a.process.on('exit', (code, signal) => {
-						eventEmitter.emit('stop', `/app/${encodeURIComponent(req.params.name || 'default')}`, { code, signal });
-					});
-					res.status(204).end();
-				});
-			})
-			.post("/app/:name/stop", (req, res, next) => {
-				if (req.params.name == 'default') req.params.name = '';
-				var a = app[req.params.name];
-				if (!a) {
-					res.sendStatus(404);
-					return;
-				}
-				if (!a.running) {
-					res.status(409).send("The app is not running.");
-					return;
-				}
-				a.stop(() => {
-					res.status(204).end();
-				});
-			})
-			.get("/module/", (req, res, next) => {
-				res.json(module);
-			})
-			.get("/module/:name", (req, res, next) => {
-				var m = module[req.params.name];
-				if (!m) {
-					res.sendStatus(404);
-					return;
-				}
-				res.json(m);
-			})
-			.post("/module/", jsonBodyParser, (req, res, next) => {
-				npm.load({ prefix: path.join(config.dir, "site_modules") }, function (er) {
-					if (er) {
-						res.status(500).send(er);
-						return;
-					}
-					npm.commands.install([req.body.source], function (er, data) {
-						if (er) {
-							res.status(500).send(er);
-							return;
-						}
-						var [, dir] = data[data.length - 1];
-						var name = path.basename(dir);
-						module[name] = req.body;
-						res.status(201).header('Location', `/module/${encodeURIComponent(name)}`).end();
-					});
-				});
-			})
-			.delete("/module/:name", jsonBodyParser, (req, res, next) => {
-				var m = module[req.params.name];
-				if (!m) {
-					res.sendStatus(404);
-					return;
-				}
-				npm.load({ prefix: path.join(config.dir, "site_modules") }, function (er) {
-					if (er) {
-						res.status(500).send(er);
-						return;
-					}
-					npm.commands.uninstall([req.params.name], function (er) {
-						if (er) {
-							res.status(500).send(er);
-							return;
-						}
-						delete module[req.params.name];
-						res.status(204).end();
-					});
-				});
-			})
-		function serialize(app) {
-			return {
-				type: app.type,
-				module: app.module,
-				arguments: app.arguments,
-				port: app.port,
-				mount: app.mount,
-				running: app.running
-			};
-		}
 		var eventEmitter = new EventEmitter();
-		adminApp
-			.get("/event/", (req, res, next) => {
-				res.writeHead(200, {
-					"Content-Type": "text/event-stream"
-				});
-				listen('start');
-				listen('stop');
-				function listen(type) {
-					eventEmitter.on(type, listener);
-					res.on('close', () => { eventEmitter.removeListener(type, listener); });
-					function listener(source, data) {
-						res.write(`event: ${type}\n`);
-						res.write(`data: ${source !== undefined ? source : ''}\n`);
-						res.write(`data: ${data !== undefined ? JSON.stringify(data) : ''}\n\n`);
-					}
-				}
-			})
+		var adminApp = new AdminApp(this, {
+			cors: fs.existsSync(path.join(config.dir, 'adminCors.json')) ?
+				cors(JSON.parse(fs.readFileSync(path.join(config.dir, 'adminCors.json'), { encoding: "utf-8" }))) : undefined,
+			auth: fs.existsSync(path.join(config.dir, 'adminBasicAuth.json')) ?
+				basicAuth(JSON.parse(fs.readFileSync(path.join(config.dir, 'adminBasicAuth.json'), { encoding: "utf-8" }))) : undefined
+		});
 		var adminServer = createServer(adminApp, config.adminSsl ? serverOptions : undefined);
 		adminServer.listen(config.adminPort);
 		var app = Storage(path.join(config.dir, 'app.json'), { constructor: this.App, destructor: app => ({ type: app.type, module: app.module, arguments: app.arguments, port: app.port, mount: app.mount }) });
@@ -409,6 +109,11 @@ class Site {
 					eventEmitter.emit('stop', `/app/${encodeURIComponent(name || 'default')}`, { code, signal });
 				});
 			});
+		this.proxyRule = proxyRule;
+		this.vhost = vhost;
+		this.app = app;
+		this.module = module;
+		this.eventEmitter = eventEmitter;
 	}
 }
 module.exports = Site;
