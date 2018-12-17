a91e0e2fae780855898edbc52a28ab69f4256b10
Jack Lu <jacklu@jacklu.me>
2018-12-17 23:28:52 +0800

    test: use port 8080 instead of 80

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ var http = require("http");
 var request = require('request');
 var Site = require('..');
 it('should start and stop', function (done) {
-	var site = new Site({ dir: "test/site/", adminPort: 9000 });
+	var site = new Site({ dir: "test/site/", port: 8080, adminPort: 9000 });
 	site.start();
-	request.get("http://localhost", (error, response) => {
+	request.get("http://localhost:8080", (error, response) => {
 		assert(response);
 		site.stop();
 		request.get("http://localhost", (error, response) => {
@@ -15,12 +15,12 @@ it('should start and stop', function (done) {
 	});
 });
 it('proxy should work', function (done) {
-	var site = new Site({ dir: "test/site/", adminPort: 9000 });
+	var site = new Site({ dir: "test/site/", port: 8080, adminPort: 9000 });
 	site.start();
 	var targetServer = http.createServer((req, res) => { res.write("42"); res.end(); })
-	targetServer.listen(8080);
-	request.put("http://localhost:9000/proxy-rule/%2fa", { json: true, body: "http://localhost:8080" }, (error, response) => {
-		request.get("http://localhost/a", (error, response) => {
+	targetServer.listen(8008);
+	request.put("http://localhost:9000/proxy-rule/%2fa", { json: true, body: "http://localhost:8008" }, (error, response) => {
+		request.get("http://localhost:8080/a", (error, response) => {
 			assert(response);
 			assert.equal(response.body, "42");
 			targetServer.close();
