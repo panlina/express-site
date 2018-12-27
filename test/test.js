7f967f10b226a3bdd21fa2e4554878db6228044c
Jack Lu <jacklu@jacklu.me>
2018-12-27 23:01:53 +0800

    test: +vhost

--- test/test.js
+++ test/test.js
@@ -29,6 +29,21 @@ it('proxy should work', function (done) {
 		});
 	});
 });
+it('vhost should work', function (done) {
+	var site = new Site({ dir: "test/site/", port: 8080, adminPort: 9000 });
+	site.start();
+	var targetServer = http.createServer((req, res) => { res.write("42"); res.end(); })
+	targetServer.listen(8008);
+	request.put("http://localhost:9000/vhost/a.localhost", { json: true, body: "http://localhost:8008" }, (error, response) => {
+		request.get("http://127.0.0.1:8080", { headers: { "Host": "a.localhost:8080" } }, (error, response) => {
+			assert(response);
+			assert.equal(response.body, "42");
+			targetServer.close();
+			site.stop();
+			done();
+		});
+	});
+});
 it('app should work', function (done) {
 	var site = new Site({ dir: "test/site/", port: 8080, adminPort: 9000 });
 	site.start();
