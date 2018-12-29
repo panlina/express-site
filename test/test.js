305bc9f2e666d80908771594134b4d6a81135798
Jack Lu <jacklu@jacklu.me>
2018-12-29 22:38:27 +0800

    test: fix: this.timeout=10000;->this.timeout(10000);

--- test/test.js
+++ test/test.js
@@ -68,7 +68,7 @@ it('app should work', function (done) {
 	});
 });
 it('module should work', function (done) {
-	this.timeout = 10000;
+	this.timeout(10000);
 	var site = new Site({ dir: "test/site/", port: 8080, adminPort: 9000 });
 	site.start();
 	request.post("http://localhost:9000/module/", { json: true, body: { source: "./a" } }, (error, response) => {
